a56c936f8a53a86b036013c1c282c50a24da04e0
anpoli99 <47492849+anpoli99@users.noreply.github.com>
2022-11-18 17:46:45 -0500

    Add files via upload

---
+++ Week 7/templates/C/C_forestfruits.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{  
+    // Make sure the class name is Main if you use this template
+    static class Edge{
+        int v, w;
+        Edge(int v, int w){
+            this.v = v;
+            this.w = w;
+        }
+    }
+    static final int V = 20000 + 5;
+    static final int INF = (int)1e9 + 5;
+    static ArrayList<Edge> adj[] = new ArrayList[V];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= v; i++) adj[i] = new ArrayList<Edge>();
+        for(int i = 0; i < e; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adj[u].add(new Edge(v,w));
+            adj[v].add(new Edge(u,w));
+            // Input format: adj[u] has a list of edges (v,w) where v is the destination vertex and w is the weight
+        }
+        // Your code to find the shortest path to gather flowers goes here
+    }
+}
